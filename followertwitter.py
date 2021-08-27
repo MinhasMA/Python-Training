c3e0b3ad9194bc74c673bf85bcfde26134e3b355
blackraven <maninder.minhas@insead.edu>
2021-08-27 13:34:54 -0500

    twitter bot

---
+++ followertwitter.py
@@ -0,0 +1,24 @@
+import selenium as sel
+from selenium import webdriver
+import time as tm
+from selenium.webdriver.common.keys import Keys
+
+
+class twitterbot:
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox()
+
+    def login(self):
+        bot = self.bot()
+        bot.get('https://twitter.com/')
+        tm.sleep(3)
+        email = bot.find_element_by_class_name('email-input')
+
+mann = twitterbot('krishsharmavan@gmail.com', 'Twitter123')
+mann.login()
+
+
+
